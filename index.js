85f4f3730892043ecc193f37d849b2bf97e508ca
DavidKozdra <david01@promoapplications.com>
2022-12-27 16:38:15 -0500

    Update: Responsive and hero section

--- index.js
+++ index.js
@@ -11,3 +11,52 @@
     document.getElementById(pageName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+  var currentSlide =0;
+  
+  var slides = document.getElementsByClassName("slide");  
+  //every 2 seconds add to the slide
+  setInterval(function() {
+    if(slides.length < 1){
+      return;
+    }
+    slides[currentSlide].style.display = "none";
+    currentSlide+=1;
+    if(currentSlide == slides.length) {
+      currentSlide = 0;
+    }
+    slides[currentSlide].style.display = "block";
+  
+  }, 2000);
+
+  setTimeout(function() {
+    document.getElementsByClassName("typewriter")[0].style.border = "none";
+  }, 1000);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    } */
