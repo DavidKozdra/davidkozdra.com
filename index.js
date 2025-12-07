e8f6f7556b7b90665ff21a8eb6f3ecbd760c9feb
David Kozdra <davidkozdra@gmail.com>
2025-12-06 22:08:30 -0500

    post mort for the old site code

--- index.js
+++ index.js
@@ -81,3 +81,42 @@ if ('serviceWorker' in navigator) {
 }else {
   console.log("PWA failure")
 }
+
+    document.getElementById("defaultOpen").click();
+
+    (function () {
+      "use strict";
+    
+      // define variables
+      var items = document.querySelectorAll(".timeline li");
+    
+      // check if an element is in viewport
+      // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+      function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <=
+            (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+      }
+    
+      function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+          if(i==0){
+            items[i].classList.add("in-view");
+          }
+
+          if (isElementInViewport(items[i])) {
+            items[i].classList.add("in-view");
+          }
+        }
+      }
+    
+      // listen for events
+      window.addEventListener("load", callbackFunc);
+      window.addEventListener("resize", callbackFunc);
+      window.addEventListener("scroll", callbackFunc);
+    })();
